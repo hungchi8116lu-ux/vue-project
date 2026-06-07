c4a10e1474332e4398b5d1dab39ab4efa9d239be
awq <hungchi8116.lu@gmail.com>
2026-06-08 02:07:01 +0800

    add base property to Vite configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Add this base property:
+  // Replace 'your-repo-name' with the exact name of your GitHub repository
+  base: '/vue-project/', 
+  
   plugins: [
     vue(),
     vueDevTools(),
